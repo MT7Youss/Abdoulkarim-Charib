621af12509970b648c9b89a536db08f3406369e2
Gabriel Charib <gabriel.charibpolls@helb-prigogine.be>
2023-01-07 15:09:14 +0100

    #################### update the manifest   add a line in the model   change views   made together with mohammed  #############

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -2,3 +2,10 @@ from odoo import fields, models, Command
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
+
+    ###########################
+    #add filds 
+    #Training Date
+    #############################
+    training_date = fields.Char(string="Training Date")
+    notes = fields.Text(string='Notes', required=False)

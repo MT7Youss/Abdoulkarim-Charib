f5cd201c4d4ad97b6787082d114ec4de644499b2
MT7Youss <mabdoulkarim@helb-prigogine.be>
2023-01-08 20:54:26 +0100

    Ajout fonction qui vérifie si la probation est nécessaire
    Dans le cadre de ce projet, dans la deuxième question, il nous est demandé de vérifier si un utilisateur peut ou non confirmer une vente ainsi que d'autre restriction

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -1,37 +1,6 @@
 from odoo import api, models, _, fields
 from odoo.exceptions import ValidationError
 from erppeek import Client
-########################################################################
-##utilisteur pas defaut 
-############################################################
-odoo = Client("http://odoo.example.com", db="mydatabase", user="user", password="password")
-
-###############################################################################
-##logique de vente d'une formation qui cree des events de calendrier 
-##########################################################################
-calendar = odoo.model("calendar.event")
-
-order = sale_order.create({
-    "partner_id": 1,
-    "order_line": [
-        (0, 0, {
-            "product_id": product_id,
-            "price_unit": price_unit,
-        })
-    ]
-})
-
-calendar = odoo.model("calendar.event")
-task = calendar.create({
-    "name": activity_calendar_event_id,
-    "start": "",
-    "stop": "2022-01-01 01:00:00",
-    "allday": False,
-    "state": "open",
-    "user_id": partner_id.company_id.name,
-})
-
-
 
 
 class SaleOrder(models.Model):
@@ -40,11 +9,14 @@ class SaleOrder(models.Model):
     # Ajoutez un champ de date de formation
     training_date = fields.Date('Date de formation')
 
-    # Ajoutez un champ d'employé
+    #Ajoutez un champ d'employé
     employee_id = fields.Many2one('hr.employee', string='Employé')
     
     #Montant
     amount = fields.Float('Montant dapprobation')
+    
+    #Déclaration d'un nouveau champ de type Many2one qui pointe vers le modèle de groupe d'Odoo (res.groups)
+    manager_group_id = fields.Many2one('res.groups', string='Groupe de gestionnaire')
 
     @api.multi
     def action_confirm(self):
@@ -62,3 +34,15 @@ class SaleOrder(models.Model):
         }
         event = Event.create(event_vals)
         return super(SaleOrder, self).action_confirm()
+    #Fonction qui vérifie si la probation est nécessaire 
+    def requires_approval(self):
+        amount = self.amount
+        manager_group = self.manager_group_id
+        if amount < 500:
+            return False
+        elif 500 <= amount < 2000:
+            return manager_group in ['group_manager_level_1', 'group_manager_level_2', 'group_manager_level_3']
+        elif 2000 <= amount < 5000:
+            return manager_group in ['group_manager_level_2', 'group_manager_level_3']
+        else:
+            return manager_group == 'group_manager_level_3'

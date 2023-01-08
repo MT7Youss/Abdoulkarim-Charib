bee6046537377392a8841c4aa5db3dd0b62191be
MT7Youss <mabdoulkarim@helb-prigogine.be>
2023-01-08 20:38:24 +0100

    Changement de BD
    Nous pensons que nous travaillions dans la mauvaise BD. Pour cela que nous travaillons dorénavant dans l'autre.

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -3,3 +3,29 @@ from odoo.exceptions import ValidationError
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+    
+    # Ajoutez un champ de date de formation
+    training_date = fields.Date('Date de formation')
+
+    # Ajoutez un champ d'employé
+    employee_id = fields.Many2one('hr.employee', string='Employé')
+    
+    #Montant
+    amount = fields.Float('Montant dapprobation')
+
+    @api.multi
+    def action_confirm(self):
+        # Récupérez les informations de la commande de vente
+        training_date = self.training_date
+        employee = self.employee_id
+
+        # Créez un événement dans le calendrier de l'employé
+        Event = self.env['calendar.event']
+        event_vals = {
+            'name': 'Formation Odoo',
+            'start_date': training_date,
+            'stop_date': training_date,
+            'employee_id': employee.id
+        }
+        event = Event.create(event_vals)
+        return super(SaleOrder, self).action_confirm()

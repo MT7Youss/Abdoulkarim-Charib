e251f5e18154947b2f38a338e8d52f9eb37d7d11
MT7Youss <mabdoulkarim@helb-prigogine.be>
2023-01-08 20:21:12 +0100

    Ajout d'événement + modif de la base de données

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -1,11 +1,27 @@
-from odoo import fields, models, Command
+from odoo import api, fields, models, Command
 
-class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
 
-    ###########################
-    #add filds 
-    #Training Date
-    #############################
-    employee = fields.Char(string="employee")
-    training_date = fields.Char(string="Training Date")
+    # Ajoutez un champ de date de formation
+    training_date = fields.Date('Date de formation')
+
+    # Ajoutez un champ d'employé
+    employee_id = fields.Many2one('hr.employee', string='Employé')
+
+    @api.multi
+    def action_confirm(self):
+        # Récupérez les informations de la commande de vente
+        training_date = self.training_date
+        employee = self.employee_id
+
+        # Créez un événement dans le calendrier de l'employé
+        Event = self.env['calendar.event']
+        event_vals = {
+            'name': 'Formation Odoo',
+            'start_date': training_date,
+            'stop_date': training_date,
+            'employee_id': employee.id
+        }
+        event = Event.create(event_vals)
+        return super(SaleOrder, self).action_confirm()
